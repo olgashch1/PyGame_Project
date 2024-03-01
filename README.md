5204a306e1d3d0bfca346a50f0abb7d0da804192
olgashch1 <156227355+olgashch1@users.noreply.github.com>
2024-03-02 00:02:05 +0300

    Загрузка проекта

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# PyGame_Project
-Игра на Python "Тетрис"
+# Tetris lite - код туториала для Proglib
+
+Статья о процессе создания игры - [Пишем Тетрис на Python с помощью библиотеки Pygame](https://proglib.io/p/pishem-tetris-na-python-s-pomoshchyu-biblioteki-pygame-2022-05-30)
+
+![tetris](https://user-images.githubusercontent.com/85797091/176393603-adace526-0014-4496-be49-817a6d747d87.png)
---
+++ settings.py
@@ -0,0 +1,123 @@
+FPS = 10
+window_w, window_h = 600, 500
+block = 20
+zone_h = 20
+zone_w = 10
+
+side_freq, down_freq = 0.1, 0.1
+
+W = 10
+H = 20
+
+side_marg = 4
+top_marg = window_h - (zone_h * block) - 5
+
+colors = ((0, 0, 225), (0, 225, 0), (225, 0, 0), (225, 225, 0))
+lightcolors = ((50, 50, 255), (50, 255, 50), (255, 30, 30), (255, 255, 30))
+white, gray, black = (255, 255, 255), (185, 185, 185), (0, 0, 0)
+
+bg_color = black
+title_color = (0, 249, 255)
+
+TILE_SIZE = 20
+
+fig_w, fig_h = 5, 5
+empty = 'o'
+
+
+figures = {'S': [['ooooo',
+                  'ooooo',
+                  'ooxxo',
+                  'oxxoo',
+                  'ooooo'],
+                 ['ooooo',
+                  'ooxoo',
+                  'ooxxo',
+                  'oooxo',
+                  'ooooo']],
+           'Z': [['ooooo',
+                  'ooooo',
+                  'oxxoo',
+                  'ooxxo',
+                  'ooooo'],
+                 ['ooooo',
+                  'ooxoo',
+                  'oxxoo',
+                  'oxooo',
+                  'ooooo']],
+           'J': [['ooooo',
+                  'oxooo',
+                  'oxxxo',
+                  'ooooo',
+                  'ooooo'],
+                 ['ooooo',
+                  'ooxxo',
+                  'ooxoo',
+                  'ooxoo',
+                  'ooooo'],
+                 ['ooooo',
+                  'ooooo',
+                  'oxxxo',
+                  'oooxo',
+                  'ooooo'],
+                 ['ooooo',
+                  'ooxoo',
+                  'ooxoo',
+                  'oxxoo',
+                  'ooooo']],
+           'L': [['ooooo',
+                  'oooxo',
+                  'oxxxo',
+                  'ooooo',
+                  'ooooo'],
+                 ['ooooo',
+                  'ooxoo',
+                  'ooxoo',
+                  'ooxxo',
+                  'ooooo'],
+                 ['ooooo',
+                  'ooooo',
+                  'oxxxo',
+                  'oxooo',
+                  'ooooo'],
+                 ['ooooo',
+                  'oxxoo',
+                  'ooxoo',
+                  'ooxoo',
+                  'ooooo']],
+           'I': [['ooxoo',
+                  'ooxoo',
+                  'ooxoo',
+                  'ooxoo',
+                  'ooooo'],
+                 ['ooooo',
+                  'ooooo',
+                  'xxxxo',
+                  'ooooo',
+                  'ooooo']],
+           'O': [['ooooo',
+                  'ooooo',
+                  'oxxoo',
+                  'oxxoo',
+                  'ooooo']],
+           'T': [['ooooo',
+                  'ooxoo',
+                  'oxxxo',
+                  'ooooo',
+                  'ooooo'],
+                 ['ooooo',
+                  'ooxoo',
+                  'ooxxo',
+                  'ooxoo',
+                  'ooooo'],
+                 ['ooooo',
+                  'ooooo',
+                  'oxxxo',
+                  'ooxoo',
+                  'ooooo'],
+                 ['ooooo',
+                  'ooxoo',
+                  'oxxoo',
+                  'ooxoo',
+                  'ooooo']]}
+
